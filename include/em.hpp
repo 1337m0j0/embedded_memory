e8437c7741fa216ab36a7d0a9e6741fc010cb822
1337m0j0 <1337m0j0@mailbox.org>
2024-03-28 15:53:49 +0100

    use namespace em

--- include/em.hpp
+++ include/em.hpp
@@ -1,16 +1,19 @@
-
-#ifndef INCLUDE_EMBEDDED_MEMORY_HPP_
-#define INCLUDE_EMBEDDED_MEMORY_HPP_
+#ifndef INCLUDE_EM_HPP_
+#define INCLUDE_EM_HPP_
 
 #include <cstddef>
+
+namespace em {
 
 /** @brief Allocates a memory region of the required size.
  *
  * Sets the complete memory region to 0 when `clear_region` is `true`.
  */
-void *EM_Alloc(const std::size_t size, const bool clear_region);
+void *alloc(const std::size_t size, const bool clear_region);
 
 /** @brief Makes a previously allocated memory region available again. */
-void EM_Free(void *ptr);
+void free(void *ptr);
 
-#endif
+}  // namespace em
+
+#endif  // INCLUDE_EM_HPP_
