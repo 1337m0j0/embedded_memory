01a2050dfb60384447a6c7e36cef5b69911a6f52
1337m0j0 <1337m0j0@mailbox.org>
2024-04-04 16:24:24 +0200

    overflow detection tests

--- tests/memory_pool_tests.cpp
+++ tests/memory_pool_tests.cpp
@@ -1,12 +1,17 @@
 #include <gtest/gtest.h>
 
+#include <array>
+#include <cstddef>
+#include <cstring>
+
 #include "src/memory_pool.hpp"
+#include "src/memory_pool_interfaces.hpp"
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////
 // Test Globals
 ////////////////////////////////////////////////////////////////////////////////////////////////////
 
-constexpr std::size_t kTestPoolBlockSize{4};
+constexpr std::size_t kTestPoolBlockSize{8};
 constexpr std::size_t kTestPoolNumBlocks{10};
 constexpr std::array<char, kTestPoolBlockSize> kClearedBlockData{};
 
@@ -41,51 +46,51 @@ TEST_F(MemoryPoolTest, GetBlockSize) {
  * requests a nullptr is returned. */
 TEST_F(MemoryPoolTest, MaximumAvailability) {
   for (std::size_t i = 0; i < kTestPoolNumBlocks; i++) {
-    void *Block = memory_pool_.GetBlock(false);
-    ASSERT_NE(Block, nullptr);
+    void *block = memory_pool_.GetBlock(false);
+    ASSERT_NE(block, nullptr);
   }
-  void *Block = memory_pool_.GetBlock(false);
-  ASSERT_EQ(Block, nullptr);
+  void *block = memory_pool_.GetBlock(false);
+  ASSERT_EQ(block, nullptr);
 }
 
 /* When a previously reserved memory Block is released, its memory becomes available again. */
 TEST_F(MemoryPoolTest, AvailableAfterRelease) {
-  void *Block = memory_pool_.GetBlock(false);
-  memory_pool_.FreeBlock(Block);
+  void *block = memory_pool_.GetBlock(false);
+  memory_pool_.FreeBlock(block);
   ASSERT_EQ(memory_pool_.GetNumFreeBlocks(), kTestPoolNumBlocks);
 }
 
-/* Trying to free an unknown memory Block, i.e. where the given pointer doesn't correspond to any
- * memory Block managed by the memory pool, the number of available memory Blocks does not change.
+/* Trying to free an unknown memory block, i.e. where the given pointer doesn't correspond to any
+ * memory block managed by the memory pool, the number of available memory blocks does not change.
  */
 TEST_F(MemoryPoolTest, ReleaseUnknownBlock) {
   std::size_t const kLocalBlockSize{10};
-  std::array<char, kLocalBlockSize> local_Block{};
-  memory_pool_.FreeBlock(local_Block.data());
+  std::array<char, kLocalBlockSize> local_block{};
+  memory_pool_.FreeBlock(local_block.data());
   ASSERT_EQ(memory_pool_.GetNumFreeBlocks(), kTestPoolNumBlocks);
 }
 
 /* Requesting a cleared memory Block will cause it to be set to zero. */
 TEST_F(MemoryPoolTest, RequestCleared) {
-  // ensure all memory Blocks have some non-zero data written to them
-  std::array<void *, kTestPoolNumBlocks> all_memory_Blocks{};
+  // ensure all memory blocks have some non-zero data written to them
+  std::array<void *, kTestPoolNumBlocks> all_memory_blocks{};
   for (std::size_t i = 0; i < kTestPoolNumBlocks; i++) {
-    all_memory_Blocks.at(i) = memory_pool_.GetBlock(false);
-    std::memset(all_memory_Blocks.at(i), 1, kTestPoolBlockSize);
+    all_memory_blocks.at(i) = memory_pool_.GetBlock(false);
+    std::memset(all_memory_blocks.at(i), 1, kTestPoolBlockSize);
   }
 
-  // no more Blocks are available
-  void *Block = memory_pool_.GetBlock(false);
-  ASSERT_EQ(Block, nullptr);
-  // free all Blocks
-  for (void *Block : all_memory_Blocks) {
-    memory_pool_.FreeBlock(Block);
+  // no more blocks are available
+  void *block = memory_pool_.GetBlock(false);
+  ASSERT_EQ(block, nullptr);
+  // free all blocks
+  for (void *block : all_memory_blocks) {
+    memory_pool_.FreeBlock(block);
   }
   ASSERT_EQ(memory_pool_.GetNumFreeBlocks(), kTestPoolNumBlocks);
-  // request a cleared Block
-  Block = memory_pool_.GetBlock(true);
-  // the whole Block shall be zeroed
-  ASSERT_EQ(std::memcmp(kClearedBlockData.data(), Block, kTestPoolBlockSize), 0);
+  // request a cleared block
+  block = memory_pool_.GetBlock(true);
+  // the whole block shall be zeroed
+  ASSERT_EQ(std::memcmp(kClearedBlockData.data(), block, kTestPoolBlockSize), 0);
 }
 
 /* Returned memory can be written to. */
@@ -94,7 +99,19 @@ TEST_F(MemoryPoolTest, WriteToMemory) {
   std::array<char, kTestPoolBlockSize> expected_data{};
   std::memset(expected_data.data(), 1, kTestPoolBlockSize);
   // write test data and check
-  void *Block = memory_pool_.GetBlock(true);
-  std::memset(Block, 1, kTestPoolBlockSize);
-  ASSERT_EQ(std::memcmp(expected_data.data(), Block, kTestPoolBlockSize), 0);
+  void *block = memory_pool_.GetBlock(true);
+  std::memset(block, 1, kTestPoolBlockSize);
+  ASSERT_EQ(std::memcmp(expected_data.data(), block, kTestPoolBlockSize), 0);
+}
+
+// A freshly-initialized memory pool shall not have buffer overflows.
+TEST_F(MemoryPoolTest, NoBufferOverflowOnInitialization) {
+  ASSERT_FALSE(memory_pool_.HasBufferOverflow());
+}
+
+// If a buffer overflow happens, it should be detected.
+TEST_F(MemoryPoolTest, BufferOverflowDetection) {
+  void *block = memory_pool_.GetBlock(true);
+  std::memset(block, 1, kTestPoolBlockSize + 1);
+  ASSERT_TRUE(memory_pool_.HasBufferOverflow());
 }
